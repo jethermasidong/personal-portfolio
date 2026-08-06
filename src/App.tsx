32f26381c7ff738b338c5afdba7506e1d5944dcb
jethermasidong <jethermasidong05@gmail.com>
2026-08-06 19:26:13 +0800

    Progress: Added Pages and Apply Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Homepage from './pages/Homepage';
+import Skill from './pages/main/Skills';
+import Projects from './pages/main/Projects';
+import Experience from './pages/main/Experience';
+
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Route element={<Layout />}>
           
           <Route path="/" element={<Homepage />} />
+
+          <Route path="/skills" element={<Skill />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
           
         
         </Route>
---
+++ src/pages/main/Skills.tsx
@@ -0,0 +1,8 @@
+export default function Skill() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
